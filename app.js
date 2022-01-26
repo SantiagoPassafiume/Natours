9faacac3bbc1fd8e7d5053885ec5cb2cf3d0a51d
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-01-26 05:13:38 -0900

    implement get request to /api/v1/tours

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// #########################################
+//  ROUTES
+// #########################################
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
 });
 
+// #########################################
+// START LISTENING FOR INCOMING REQUESTS
+// #########################################
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+// #########################################
