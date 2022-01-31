853d58e72798474e6718dd422dfa31dfeefd92f5
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-01-31 13:40:27 -0900

    add middleware to check the body of a POST request to /api/v1/tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,6 +16,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid parameters to create a new Tour',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
